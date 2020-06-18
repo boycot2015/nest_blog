da730d366ca0156d3ed0e6c405f4c953bd4b2790
zch <boycot2017@163.com>
2020-06-18 18:19:13 +0800

    处理样式

--- client/mobile/src/pages/article/index.vue
+++ client/mobile/src/pages/article/index.vue
@@ -18,7 +18,7 @@
 				</view>
 			</uni-card>
 		</view>
-		<uni-load-more :status="status" :icon-size="16" />
+		<uni-load-more :status="status" @clickLoadMore="clickLoadMore" :icon-size="16" />
 	</view>
 </template>
 
@@ -28,23 +28,55 @@
 			return {
 				viewList: [],
 				status: 'loading',
-				contentText: ''
+				contentText: '',
+				current: 1,
+				pageSize: 2,
+				total: 0
 			}
 		},
 		onLoad() {
 			this.init()
 		},
 		onPullDownRefresh() {
+			this.current = 1
 			this.init()
 		},
+		onReachBottom() {
+			//触底的时候请求数据，即为上拉加载更多
+			if (this.viewList.length === this.total) return
+			this.current += 1
+			this.status = 'more'
+			const { current, pageSize } = this 
+			this.init({ current, pageSize })
+		},
 		methods: {
-			async init () {
+			async init (optipns) {
+				optipns = optipns || {
+					current: this.current,
+					pageSize: this.pageSize
+				}
 				this.status = 'loading'
-				let res = await this.$api.article.get()
+				let res = await this.$api.article.get(optipns)
 				if (res && res.success) {
-					console.log(res.data[0], 'res.data[0]')
-					this.viewList = res.data[0]
-					this.status = 'more'
+					// console.log(res.data[0], 'res.data[0]')
+					if (this.current > 1) {
+						setTimeout(() => {
+							this.viewList = this.viewList.concat(res.data[0])
+							this.total = res.data[1]
+							this.status = 'more'
+							if (this.viewList.length === res.data[1]) {
+								this.status = 'noMore'
+							}
+						}, 1000)
+						
+					} else {
+						this.viewList = res.data[0]
+						this.total = res.data[1]
+						this.status = 'more'
+						if (this.viewList.length === res.data[1]) {
+							this.status = 'noMore'
+						}
+					}
 					setTimeout(function() {
 						uni.stopPullDownRefresh()
 					}, 1000)
@@ -55,6 +87,11 @@
 					url: '/pages/article/view?id='+ item.id
 				})
 				// this.$router.push('/pages/webview?url='+ item.link)
+			},
+			clickLoadMore ({ detail }) {
+				const { status } = detail
+				this.current += 1
+				status === 'more' && this.init()
 			}
 		}
 	}
