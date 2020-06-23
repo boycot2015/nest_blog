1525a93951b3904f55511e0150629b59eccc7b3b
zch <boycot2017@163.com>
2020-06-23 15:31:25 +0800

    api接口地址修改为域名

--- client/mobile/src/api/baseUrl.js
+++ client/mobile/src/api/baseUrl.js
@@ -1,14 +1,15 @@
-let baseUrl = 'http://106.13.8.33:4000/'
+let baseUrl = 'http://106.13.8.33:4000'
 
 const env = process.env.NODE_ENV
 console.log(env, 'env')
 switch (env) {
     case 'development':
         // baseUrl = 'http://192.168.0.111:4000';
-        // baseUrl = 'http://192.168.1.175:4000';
+        baseUrl = 'http://106.13.8.33:4000';
         break;
     case 'production':
-        baseUrl = 'http://106.13.8.33:4000'
+        // baseUrl = 'http://106.13.8.33:4000'
+        baseUrl = 'http://www.blog.api.boycot.top'
         break;
     default:
         break;
